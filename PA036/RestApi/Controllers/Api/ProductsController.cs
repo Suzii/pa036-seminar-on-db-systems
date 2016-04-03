a23598e0826dadc53b8df6274a7f63c22f30873e
Suzii <z.dankovcikova@gmail.com>
2016-04-03 12:31:04 +0200

    Products controller update

--- PA036/RestApi/Controllers/Api/ProductsController.cs
+++ PA036/RestApi/Controllers/Api/ProductsController.cs
@@ -1,29 +1,74 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Web.Http;
+using System.Web.Http.Routing;
+using Service.Data;
+using Service.DTO;
+using Shared.Modifiers;
 
 namespace RestApi.Controllers.Api
 {
     public class ProductsController : ApiController
     {
-        // GET: api/Products
-        public IEnumerable<string> Get()
+        private readonly IProductService _productService;
+
+        public ProductsController()
         {
-            return new string[] { "value1", "value2" };
+            _productService = new ProductService();
+        }
+
+        // GET: api/Products
+        public IEnumerable<ProductDTO> Get(int page = 0, int pageSize = 10)
+        {
+            var totalCount = 100;//query.Count();
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+            var urlHelper = new UrlHelper(Request);
+            var prevLink = page > 0 ? urlHelper.Link("Products", new { page = page - 1, pageSize = pageSize }) : "";
+            var nextLink = page < totalPages - 1 ? urlHelper.Link("Products", new { page = page + 1, pageSize = pageSize }) : "";
+
+            var paginationHeader = new
+            {
+                TotalCount = totalCount,
+                TotalPages = totalPages,
+                PrevPageLink = prevLink,
+                NextPageLink = nextLink
+            };
+
+            System.Web.HttpContext.Current.Response.Headers.Add("X-Pagination", Newtonsoft.Json.JsonConvert.SerializeObject(paginationHeader));
+
+            var modifier = new ProductModifier()
+            {
+                Skip = page,
+                Take = pageSize
+            };
+
+            var result = _productService.Get(modifier);
+            return result;
         }
 
         // GET: api/Products/5
-        public string Get(int id)
+        public ProductDTO GetProduct(int id)
         {
-            return "value";
+            var modifier = new ProductModifier()
+            {
+                Ids = new int[] {id}
+            };
+
+            var result = _productService.Get(modifier).SingleOrDefault();
+            return result;
         }
 
         // POST: api/Products
-        public void Post([FromBody]string value)
+        public void Post([FromBody]ProductDTO product)
         {
+            _productService.Create(product);
         }
 
         // PUT: api/Products/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]ProductDTO product)
         {
         }
 
