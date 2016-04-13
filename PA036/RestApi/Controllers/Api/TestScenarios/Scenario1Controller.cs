bc89fe6f4623189443a9c5961dadd5315d36801c
roman <stone99@azet.sk>
2016-04-13 21:46:20 +0200

    Pridany prvy test co aj realne daco robi

--- PA036/RestApi/Controllers/Api/TestScenarios/Scenario1Controller.cs
+++ PA036/RestApi/Controllers/Api/TestScenarios/Scenario1Controller.cs
@@ -1,14 +1,50 @@
 ﻿using System.Threading.Tasks;
+using Service.Data;
+using System;
+using System.Diagnostics;
 using System.Web.Http;
+using System.Collections;
+using System.Collections.Generic;
+using Shared.Settings;
+using Shared.Filters;
+using Service.Config;
 
 namespace RestApi.Controllers.Api.TestScenarios
 {
     public class Scenario1Controller : ApiController
     {
-        public async Task<string> Get()
+        private IProductService _instance = new ProductService();
+        private IDatabaseService _dbInstance = new DatabaseService();
+
+        public async Task<IDictionary<string, IList<double>>> Get()
         {
-            return
-                "This is an example of a TestScenario controller that executes the test and returns statistics as a json object.";
+
+            var dbSettings = new DbSettings() { UseSecondAppContext = true};
+            _dbInstance.InvalidateCache();
+            var totalCount = await _instance.TotalCountAsync(dbSettings);
+            var modifier = new ProductFilter();
+            var output = new Dictionary<string, IList<double>>();
+            var withoutCache = new List<double>();
+            var withCache = new List<double>();
+            const int step = 1000;
+
+            for (var i = step; i <= totalCount; i += step)
+            {
+                modifier.Take = i;
+                var watch = Stopwatch.StartNew();
+                await _instance.GetAsync(modifier, dbSettings);
+                watch.Stop();
+                withoutCache.Add(watch.ElapsedMilliseconds / 1000.0);
+
+                watch.Restart();
+                await _instance.GetAsync(modifier, dbSettings);
+                watch.Stop();
+                withCache.Add(watch.ElapsedMilliseconds / 1000.0);
+            }
+            output.Add("notCached", withoutCache);
+            output.Add("cached", withCache);
+            output.Add("xAxis", new List<double>() { 0, totalCount + step, step });
+            return output;
         }
 
     }
